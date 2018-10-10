f8b08d2517196be275bc2c6a1db4d7b7c178f877
Sage Vogt <thurmanvogt@gmail.com>
2018-10-10 17:50:07 -0600

    Add burger menu to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu'
 import './App.css';
 
+
 class App extends Component {
+
+  showSettings (event) {
+    event.preventDefault();
+  
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header"> 
-          <h1 className="App-job">Software Developer</h1>
-          <h1 className="App-name">Sage L. Vogt</h1>
-        </header>
-        <nav className="App-nav"> 
-          <ul className="App-nav--list">
-            <li>contact me</li>
-            <li>projects</li>
-            <li>resume</li>
-          </ul>
-        </nav>
-
-
+        <Menu 
+          right
+          pageWrapId={"page-wrap"} 
+          outerContainerId={"App"}>
+            <a id="home" className="menu-item" href="/">Home</a>
+            <a id="about" className="menu-item" href="/about">About</a>
+            <a id="contact" className="menu-item" href="/contact">Contact</a>
+            <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        </Menu>
+        <section id="page-wrap">
+          <header className="App-header">
+            <h1 className="App-name">Sage L. Vogt</h1>
+            <h1 className="App-job">Software Developer</h1>
+          </header>
+          <nav className="App-nav">
+            <ul className="App-nav--list">
+              <li><a>contact me</a></li>
+              <li><a>projects</a></li>
+              <li><a>resume</a></li>
+            </ul>
+          </nav>
+        </section>
       </div>
     );
   }
